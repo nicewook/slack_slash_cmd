a8e0a08bc87afca21127e7ce07484b1354bf5008
Jeong Hyunseok <nicewook@hotmail.com>
2019-02-12 19:39:59 +0900

    version2. httprouter

---
+++ version2/server2.go
@@ -0,0 +1,26 @@
+// reference link: https://github.com/nlopes/slack/blob/master/examples/slash/slash.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/nicewook/slack_slash_cmd/slash"
+)
+
+func index() {
+	fmt.Fprint(w, "Welcome, I'm Timebot\n\nI can convert KST <-> PST/PDT")
+}
+
+func main() {
+
+	r := httprouter.New()
+	r.GET("/", index)
+	r.POST("/slash", slash.Handler)
+
+	fmt.Println("[INFO] Server listening")
+	log.Fatal(http.ListenAndServe("", r))
+
+}
---
+++ version2/slash/handlers.go
@@ -0,0 +1,68 @@
+package slash
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// SlashCommand is Slack slash command struct
+type SlashCommand struct {
+	Token          string `json:"token"`
+	TeamID         string `json:"tead_id"`
+	TeamDomain     string `json:"team_domain"`
+	EnterpriseID   string `json:"enterprise_id,omitempty"`
+	EnterPriseName string `json:"enterprise_name,omitempty"`
+	ChannelID      string `json:"channel_id"`
+	ChannelName    string `json:"channel_name"`
+	UserID         string `json:"user_id"`
+	UserName       string `json:"user_name"`
+	Command        string `json:"command"`
+	Text           string `json:"text"`
+	ResponseURL    string `json:"response_url"`
+	TirggerID      string `json:"trigger_id"`
+}
+
+func slashCommandParse(r *http.Request) (s SlashCommand, err error) {
+	if err = r.ParseForm(); err != nil {
+		return s, err
+	}
+	s.Token = r.PostForm.Get("token")
+	s.TeamID = r.PostForm.Get("team_id")
+	s.TeamDomain = r.PostForm.Get("team_domain")
+	s.EnterpriseID = r.PostForm.Get("enterprise_id")
+	s.EnterPriseName = r.PostForm.Get("enterprise_name")
+	s.ChannelID = r.PostForm.Get("channel_id")
+	s.ChannelName = r.PostForm.Get("channel_name")
+	s.UserID = r.PostForm.Get("user_id")
+	s.UserName = r.PostForm.Get("user_name")
+	s.Command = r.PostForm.Get("command")
+	s.Text = r.PostForm.Get("text")
+	s.ResponseURL = r.PostForm.Get("response_url")
+	s.TirggerID = r.PostForm.Get("trigger_id")
+
+	return s, nil
+}
+
+// Handler deals with slash commands
+func Handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s, err := slashCommandParse(r)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: validateToken
+
+	switch s.Command {
+	case "/time":
+		response := fmt.Sprintf("You requested for KST <-> PST/PDT for %#v", s)
+		w.Write([]byte(response))
+
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
